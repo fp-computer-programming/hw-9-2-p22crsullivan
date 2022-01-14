c6119dfe665be392d1ba26ae2b57f00e183dbb8d
p22crsullivan <p22crsullivan@fairfieldprep.org>
2022-01-14 10:02:39 -0500

    Save HW 9-2

--- hw9-2-1.py
+++ hw9-2-1.py
@@ -3,10 +3,17 @@ last_initials = ["B", "D", "H", "E", "G", "G", "H", "R", "M", "L", "I", "I", "N"
 rows = [["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan"], ["Sean", "Luis", "Ryan", "James", "Jack"],
 ["Aiden", "Ian", "Colin", "Tim", "Cam"],
 ["Larry", "Spencer", "Chris", "Ryan", "Nolan"]]
-
+initial_number = 0
+name_number = 0
 for row in rows:
     for i, v in enumerate(rows):
-        if i.index == v.index:
-            i.append(last_initials)
+        if name_number == initial_number:
+            rows = rows[:20] + last_initials
+            initial_number += 1
+            name_number += 1
+            print(rows)
+            break
+        elif rows.index != initial_number:
+            break
         
 print(rows)
